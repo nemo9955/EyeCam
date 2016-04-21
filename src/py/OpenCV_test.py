c80a1e8a24e335cbd62cd1628ea04685a4e769d2
Adrian Mogoi <nemo9955@gmail.com>
2016-04-21 17:26:41 +0300

    made openCV work

--- src/py/OpenCV_test.py
+++ src/py/OpenCV_test.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
+
+
 import numpy as np
 import cv2
 
-# Load an color image in grayscale
-img = cv2.imread('../res/people.jpg',0)
+cap = cv2.VideoCapture(0)
+
+while(1):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, 0)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+print "done"
